d484d9919792391a1dd6bb1ce8b8d8f4c23a8ae6
Okyere Osei-Akoto <okyereworkmail22@gmail.com>
2023-07-24 10:54:21 +0000

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -12,13 +12,45 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
       const button = document.querySelector(`.${buttonClass}`);
       if (button) {
         button.click();
+        console.log('Clicked');
       } else {
         console.error(`Button with class '${buttonClass}' not found.`);
       }
     }
 
+    // Function to scroll the mouse randomly within the viewport.
+    function scrollPageRandomly() {
+      const direction = Math.random() < 0.5 ? -1 : 1;
+      const randomScrollAmount = Math.floor(Math.random() * 300) * direction;
+    
+      window.scrollBy(0, randomScrollAmount);
+      console.log('Scrolled');
+    }
+
+    // Function to move the mouse randomly within the viewport.
+    function moveMouseRandomly() {
+      const viewport = document.documentElement;
+      const viewportWidth = viewport.clientWidth;
+      const viewportHeight = viewport.clientHeight;
+
+      const randomX = Math.floor(Math.random() * viewportWidth);
+      const randomY = Math.floor(Math.random() * viewportHeight);
+
+      const event = new MouseEvent("mousemove", {
+        clientX: randomX,
+        clientY: randomY,
+      });
+
+      document.dispatchEvent(event);
+      console.log('Mouse moved');
+    }
+
     // Start auto-clicking at the specified interval
     const autoClickInterval = setInterval(clickTargetButton, interval * 1000);
+    // Start auto-scrolling at the specified interval
+    const autoScrollInterval = setInterval(scrollPageRandomly, (interval) * 1000);
+    // Start auto-mouse move at the specified interval
+    const autoMouseMoveInterval = setInterval(moveMouseRandomly, (interval) * 1000);
 
     // Send response to the background script
     sendResponse({ started: true });
